76c7b92f56d33bc7eeee17b21201a7523990b046
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2024-06-26 15:38:34 +0530

    made sidebar responsive

--- client/src/pages/components/Sidebar.tsx
+++ client/src/pages/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import MenuIcon from "@mui/icons-material/Menu";
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 function Sidebar() {
   const BASE_URL = "http://localhost:5000";
@@ -62,7 +62,7 @@ function Sidebar() {
 
   return (
     <>
-      <div className="p-[15px] w-[25%] h-[100vh] flex flex-col justify-between bg-white shadow-xl">
+      <div className="hidden lg:block p-[15px] w-[25%] h-[100vh] flex flex-col justify-between bg-white shadow-xl">
         <div>
           <div className="p-[5px] my-[10px] text-[25px] bg-green-700 font-bold text-white text-center cursor-default">
             Split - your - bills
@@ -79,7 +79,11 @@ function Sidebar() {
               <Link href={`/${username}/${bill.title}/${bill._id}`} key={index}>
                 <div
                   key={index}
-                  className={`border-b-[1px] border-[rgb(141,141,141)] text-center py-[5px] px-[10px] my-[10px] ${bill.title === billname ? "text-green-600 font-bold text-[20px]" : "text-[14px] text-gray-600"} flex items-center justify-between cursor-pointer`}
+                  className={`border-b-[1px] border-[rgb(141,141,141)] text-center py-[5px] px-[10px] my-[10px] ${
+                    bill.title === billname
+                      ? "text-green-600 font-bold text-[20px]"
+                      : "text-[14px] text-gray-600"
+                  } flex items-center justify-between cursor-pointer`}
                 >
                   <div className="">{bill.title}</div>
                   {bill.title === billname && <ArrowRightIcon />}
@@ -93,49 +97,62 @@ function Sidebar() {
           <div className="flex items-center cursor-pointer" title="Log out">
             {/* <img src="" alt="" /> */}
             <div className="text-[16px] font-bold text-black mx-[10px]">
-              Vikash
+            Log Out ({user.name})
             </div>
           </div>
         </Link>
       </div>
 
-      <div className="lg:hidden absolute z-10 p-[5px]">
+      <div className="lg:hidden absolute z-10">
         <MenuIcon
-          style={{ fontSize: "26px", cursor: "pointer" }}
+          style={{ fontSize: "35px", cursor: "pointer", padding: "5px" }}
           onClick={() => setIsTrue(!isTrue)}
         />
         {isTrue && (
-          <div className="bg-[rgb(41,41,41)] p-[20px] w-[300px] min-h-[500px] text-white flex flex-col justify-between">
+          <div className="bg-white p-[20px] w-[100vw] h-[96vh] text-white flex flex-col justify-between">
             <div>
-              <div className="p-[5px] my-[10px] text-[12px] bg-[rgb(195,195,195)] text-center cursor-default">
+              <div className="p-[5px] my-[10px] text-[18px] bg-green-700 font-bold text-white text-center cursor-default">
                 Split - your - bills
               </div>
 
               <div>
-                <div
-                  className="w-[100%] h-[25px] text-white p-[5px] mt-[10px] mb-[20px] text-[10px] font-bold cursor-pointer flex items-center justify-center rounded-md border-[1px] duration-200 hover:text-[22px] hover:text-[rgb(0,144,72)]"
-                  onClick={() => {
-                    setIsTrue(!isTrue);
-                  }}
-                >
-                  + New
-                </div>
-
-                <div className="border-b-[1px] border-[rgb(141,141,141)] text-center py-[5px] px-[10px] my-[10px] text-[10px] text-white flex items-center justify-between cursor-pointer">
-                  <div className="text-[rgb(0,144,72)] font-bold hover:text-[rgb(255,255,216)]">
-                    Jammu & Kashmir
+                <Link href={`/${username}/create-new-bill`}>
+                  <div
+                    className="w-[100%] h-[40px] text-black p-[5px] mt-[30px] mb-[20px] text-[15px] font-bold cursor-pointer flex items-center justify-center rounded-md border-[1px] duration-200 hover:text-[rgb(0,144,72)] hover:border-green-600"
+                    onClick={() => {
+                      setIsTrue(!isTrue);
+                    }}
+                  >
+                    + Add New
                   </div>
-                  {/* <div className="hover:text-[rgb(255,255,216)]">{bill.title}</div> */}
-                  {/* <div className="text-[7px] text-[rgb(185,185,185)]">{new Date().toLocaleDateString()}</div> */}
-                </div>
+                </Link>
+
+                {bills?.map((bill: any, index) => (
+                  <Link
+                    href={`/${username}/${bill.title}/${bill._id}`}
+                    key={index}
+                  >
+                    <div
+                      key={index}
+                      className={`border-b-[1px] border-[rgb(141,141,141)] text-center py-[5px] px-[10px] my-[12px] ${
+                        bill.title === billname
+                          ? "text-green-600 font-bold text-[20px]"
+                          : "text-[14px] text-gray-600"
+                      } flex items-center justify-between cursor-pointer`}
+                    >
+                      <div className="">{bill.title}</div>
+                      {bill.title === billname && <ArrowRightIcon />}
+                    </div>
+                  </Link>
+                ))}
               </div>
             </div>
 
             <Link href="/login">
               <div className="flex items-center cursor-pointer" title="Log out">
                 {/* <img src={user?.profilePic} alt="" /> */}
-                <div className="text-[10px] font-bold text-white mx-[10px]">
-                  {user.name}
+                <div className="text-[13px] font-bold text-black mx-[10px]">
+                  Log Out ({user.name})
                 </div>
               </div>
             </Link>
