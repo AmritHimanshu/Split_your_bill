b06963e3dd3482424905cd822ee16f576f065979
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2024-09-08 12:22:06 +0530

    added loading

--- client/src/pages/[username]/create-new-bill.tsx
+++ client/src/pages/[username]/create-new-bill.tsx
@@ -99,7 +99,6 @@ function AddNewBill() {
       if (res.status === 200) {
         setIsLoading(false);
         window.alert("Bill created");
-        console.log(data);
         router.push(`/${username}/${data.title}/${data._id}`);
       }
     } catch (error) {
