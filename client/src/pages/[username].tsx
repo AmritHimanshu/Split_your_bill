bc3a52381d462f02ed427db54b796bc9ba7c882f
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-06-18 13:05:34 +0530

    Making files more organized

--- client/src/pages/[username].tsx
+++ client/src/pages/[username].tsx
@@ -7,12 +7,11 @@ import Sidebar from "./components/Sidebar";
 export default function Home() {
 
   const userState = useAppSelector((state) => state.user.userState);
-  console.log(userState)
 
   const router = useRouter();
 
   useEffect(() => {
-    // if (!userState) router.push(LOGIN);
+    if (!userState) router.push(LOGIN);
   }, []);
 
   return (
